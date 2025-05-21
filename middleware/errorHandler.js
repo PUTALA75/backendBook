a641f683512aa065195f1f801e728fac19977ada
PUTALA75 <balaputala@gmail.com>
2025-05-22 00:21:06 +0530

    Update errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,3 +1,10 @@
+/*const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+};
+
+module.exports = errorHandler;*/
+
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Internal Server Error" });
