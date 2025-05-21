804b37799a967ebdb675eddd17b39bca75922977
PUTALA75 <balaputala@gmail.com>
2025-05-22 00:20:27 +0530

    Update bookmodel.js

--- models/bookmodel.js
+++ models/bookmodel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+/*const mongoose = require("mongoose");
 
 const BookSchema=mongoose.Schema({
     title:{
@@ -18,4 +18,15 @@ const BookSchema=mongoose.Schema({
 
 const BooksData=mongoose.model("book",BookSchema)
 
-module.exports=BooksData;
+module.exports=BooksData;*/
+const mongoose = require("mongoose");
+
+const BookSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  publishedYear: { type: Date, required: true }
+});
+
+const BooksData = mongoose.model("Book", BookSchema);
+
+module.exports = BooksData;
